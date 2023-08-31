5bf069f507582656ce041f3f9a60cb6d43bc2043
unknown <kipkemoiedgah@gmail.com>
2023-08-31 11:33:41 +0300

    Add react routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
+import Quotes from './components/Quotes';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="quotes" element={<Quotes />} />
+      </Routes>
+
+    </>
+
   );
 }
 
